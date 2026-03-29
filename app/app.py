472a50ba5cefa7ac103a0ad844ca3aa8219b8f62
Fresnelamh <fresnel.amahowe@gmail.com>
2026-03-29 15:53:01 +0200

    test pipeline cassé

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app
 
 @app.route("/")
 def home():
