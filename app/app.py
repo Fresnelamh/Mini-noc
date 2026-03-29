de0c89fe2ea1112571db9a93115fde4c3a2fc2e5
Fresnelamh <fresnel.amahowe@gmail.com>
2026-03-29 15:36:52 +0200

    premier commit : infra de base + pipeline CI/CD

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route("/")
+def home():
+    return jsonify({"status": "ok", "message": "App is running"})
+
+@app.route("/data")
+def data():
+    return jsonify({"users": 42, "requests_today": 1337})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
